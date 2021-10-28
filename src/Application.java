26cca807d2eb946f4954d22a0229ad7b90984ee3
kibum0405 <kibum0405@uengine.org>
2021-10-28 17:34:41 +0900

    선택된 동물에 따른 출력

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,13 @@
 public class Application {
     public static void main(String args[]) {
-        System.out.println("Hello, world");
+        System.out.println("선택한 동물은:" + args[0]);
+
+        if("Dog".equals(args[0])) {
+            System.out.println("멍멍");
+        } else if("Cat".equals(args[0])) {
+            System.out.println("냐옹"); 
+        } else if("Chicken".equals(args[0])) {
+            System.out.println("꼬꼬댁");
+        }
     } 
 }
