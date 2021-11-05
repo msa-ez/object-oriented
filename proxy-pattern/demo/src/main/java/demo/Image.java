47343c020276dc6b55cce284cbf465140c856126
kibum0405 <kibum0405@uengine.org>
2021-11-05 16:43:12 +0900

    proxy-pattern init

---
+++ proxy-pattern/demo/src/main/java/demo/Image.java
@@ -0,0 +1,5 @@
+package demo;
+
+interface Image {
+    public void displayImage();
+}
---
+++ proxy-pattern/demo/src/main/java/demo/RealImage.java
@@ -0,0 +1,33 @@
+//Chapter .1
+// implements는 구현해서 넣었다는 뜻입니다.
+// 인터페이스란 구현체가 하나도 없이 methods 선언만 있는 것이 인터페이스입니다.
+// RealImage는 filename을 가지고 하나가 생성이 될 때 마다 그것을 loadImageFromDisk 에서 로딩하는 로직이 항상 동반됩니다.
+
+package demo;
+
+class RealImage implements Image {
+    private final String filename;
+
+    /**
+     * Constructor
+     * @param filename
+     */
+    public RealImage(String filename) {
+        this.filename = filename;
+        loadImageFromDisk();
+    }
+
+    /**
+     * Loads the image from the disk
+     */
+    private void loadImageFromDisk() {
+        System.out.println("Loading   " + filename);
+    }
+
+    /**
+     * Displays the image
+     */
+    public void displayImage() {
+        System.out.println("Displaying " + filename);
+    }
+}
